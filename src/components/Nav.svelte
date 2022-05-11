4839d7e23eacd96370cc0604ae2e26cc0ebe0f7c
David <djaime93@gmail.com>
2022-05-10 21:35:24 -0500

    Installed Tailwind and add nav component

---
+++ src/components/Nav.svelte
@@ -0,0 +1,12 @@
+<script>
+    const style = {
+        nav: `flex flex-1 justify-center justify-start bg-purple-100`
+    }
+</script>
+
+<div class={style.nav}>
+    <img src="favicon.png" alt="">
+    <div></div>
+    <div></div>
+    <div></div>
+</div>
